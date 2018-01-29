6b96f61c2b6a03af38c5ac1ff895b1b37a6669f3
louis <louis_boyle@hotmail.com>
2018-01-29 19:43:39 +0000

    commented react, just using the js console, basics in place

--- 03-redux/src/index.js
+++ 03-redux/src/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/normalize.css';
-import './styles/skeleton.css';
-import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles/normalize.css';
+// import './styles/skeleton.css';
+// import './styles/index.css';
+// import App from './components/App';
+// import registerServiceWorker from './registerServiceWorker';
 
 import { createStore } from 'redux';
 
 const reducer = (state, action) => {
   switch(action.type) {
     case "ADD":
+      state = state + action.payload;
       break;
     case "SUBTRACT":
+    state = state - action.payload;
       break;
   }
   return state;
 };
 
-// const store = createStore(, 1);
+const store = createStore(reducer, 1);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+store.subscribe(() => {
+  console.log("store updated", store.getState());
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+store.dispatch({
+  type: "ADD",
+  payload: 100
+});
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 40
+});
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
