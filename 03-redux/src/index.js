d4f86c14a22650278bd402d83c3058048a6248d3
louis <louis_boyle@hotmail.com>
2018-01-29 19:37:16 +0000

    install redux and make a start

--- 03-redux/src/index.js
+++ 03-redux/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/normalize.css';
+import './styles/skeleton.css';
+import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "ADD":
+      break;
+    case "SUBTRACT":
+      break;
+  }
+  return state;
+};
+
+// const store = createStore(, 1);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
